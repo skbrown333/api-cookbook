ff73332e1652da006acf16dad33734930c101d49
skbrown333 <skbrown333@gmail.com>
2021-05-26 09:30:30 -0500

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,14 @@ import express from "express";
 import { handleError } from "../utils/utils";
 
 import { default as CookbookRouter } from "./Cookbook";
+import { default as CharacterRouter } from "./Character";
+import { default as GameRouter } from "./Game";
 
 const router = express.Router();
 
 router.use("/cookbooks", CookbookRouter);
-router.use("/", (req, res) => {
-  res.send("hello world");
-});
+router.use("/characters", CharacterRouter);
+router.use("/games", GameRouter);
 router.use(handleError);
 
 export default router;
