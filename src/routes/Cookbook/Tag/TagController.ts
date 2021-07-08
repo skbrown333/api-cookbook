e6622197d1eaea551f346ba4da2308896d54320d
skbrown333 <skbrown333@gmail.com>
2021-07-08 09:56:12 -0500

    feat(TagController): add tag delete logic

--- src/routes/Cookbook/Tag/TagController.ts
+++ src/routes/Cookbook/Tag/TagController.ts
@@ -1,4 +1,6 @@
+import createError from 'http-errors';
 import BaseController from '../../../base/BaseController';
+import { PostModel } from '../../../models/Post/post.model';
 import { TagModel } from '../../../models/Tag/tag.model';
 
 class TagController extends BaseController {
@@ -10,6 +12,25 @@ class TagController extends BaseController {
     options.populateFields = 'cookbook';
     options.routeSingular = 'tag';
     super(options);
+  }
+
+  async delete(req, res, next) {
+    if (!req.params || !req.params[this.routeSingular]) {
+      return next(createError(500, 'Missing required params'));
+    }
+
+    const modelId = req.params[this.routeSingular];
+    const posts = await PostModel.find({ tags: { $all: [modelId] } });
+
+    for (let i = 0; i < posts.length; i++) {
+      const post = posts[i];
+      post.tags = post.tags.filter((p: any) => {
+        if (p._id.toString() !== modelId) return p;
+      });
+      await post.save();
+    }
+
+    return await super.delete(req, res, next);
   }
 }
 
