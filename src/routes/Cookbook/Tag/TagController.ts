3292fc308824e93b6953dc9a810b37b2f622742d
skbrown333 <skbrown333@gmail.com>
2021-07-08 10:06:02 -0500

    fix(TagController): remove tags from guides

--- src/routes/Cookbook/Tag/TagController.ts
+++ src/routes/Cookbook/Tag/TagController.ts
@@ -2,6 +2,7 @@ import createError from 'http-errors';
 import BaseController from '../../../base/BaseController';
 import { PostModel } from '../../../models/Post/post.model';
 import { TagModel } from '../../../models/Tag/tag.model';
+import { GuideModel } from '../../../models/Guide/guide.model';
 
 class TagController extends BaseController {
   constructor() {
@@ -21,13 +22,24 @@ class TagController extends BaseController {
 
     const modelId = req.params[this.routeSingular];
     const posts = await PostModel.find({ tags: { $all: [modelId] } });
+    const guides = await GuideModel.find({ tags: { $all: [modelId] } });
 
+    // Remove tag from posts
     for (let i = 0; i < posts.length; i++) {
       const post = posts[i];
       post.tags = post.tags.filter((p: any) => {
         if (p._id.toString() !== modelId) return p;
       });
       await post.save();
+    }
+
+    // Remove tag from guides
+    for (let i = 0; i < guides.length; i++) {
+      const guide = guides[i];
+      guide.tags = guide.tags.filter((p: any) => {
+        if (p._id.toString() !== modelId) return p;
+      });
+      await guide.save();
     }
 
     return await super.delete(req, res, next);
