4d546a89d0ede119a09b8337208ba2f42082d68b
skbrown333 <skbrown333@gmail.com>
2021-10-25 13:15:02 -0500

    test

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -172,7 +172,7 @@ export const getSessionCookie = async (req, res, next) => {
     httpOnly: true,
     secure: true,
     sameSite: 'strict',
-    domain: `.${process.env.CORS}`,
+    domain: `.dev-cookbook.com`,
   };
   res.cookie('session', cookie, options);
   return res.end(JSON.stringify({ status: 'success' }));
